65fcb58809d1b241a63ec7ecf65f2d6d3ae3410a
mayank0508 <thakurmayank0508@gmail.com>
2021-12-12 13:09:05 +0530

    complete

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,6 +1,6 @@
 const Interval = setInterval(() => {
     console.log('Sending notification...')
-}, 2000);
+}, 1000);
 
 
 document.getElementById('stopi-btn').addEventListener('click', () => { // this code is used to stop the interval 
